3633bd64b3f289763b7b04d2962f04a60c848630
Hare-Ball <robby.hare@gmail.com>
2021-07-08 16:02:31 -0500

    include real README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {
+//   const MIT = ["MIT"]
+//   const IBM = ["IPL%201.0"]
+//   const Apache = ["Apache%202.0"]
+//   const Mozilla =["MPL%202.0"]
+//   const None = []
+//   return license
+//}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,13 +24,13 @@ function generateMarkdown(data) {
 ${data.description}
 
 # Table of Contents
-*[Title](#Title)
-*[Installation](#Installation)
-*[Usage Information](#Usage)
-*[Contributors](#Contributors)
-*[Testers](#Testers)
-*[License](#License)
-*[GitHub](#GitHub)
+1. [Title](#Title)
+2. [Installation](#Installation)
+3. [Usage Information](#Usage)
+4. [Contributors](#Contributors)
+5. [Testers](#Testers)
+6. [License](#License)
+7. [GitHub](#GitHub)
 
 # Installation
 ${data.installInstruction}
@@ -38,7 +45,7 @@ ${data.contributors}
 ${data.tests}
 
 # License
-[![License](https://img.shields.io/badge/License-${data.licenses}-blue.svg)]
+[![License](https://img.shields.io/badge/License-${data.licenses}-green.svg)]
 This is application is covered under the ${data.licenses} license.
 
 # Questions?
