86d2f34ab7ec381eea814cd56a8f849a126f1878
Hare-Ball <82181590+Hare-Ball@users.noreply.github.com>
2021-07-08 22:43:04 -0500

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// function renderLicenseBadge(license) {
-//   const MIT = ["MIT"]
-//   const IBM = ["IPL%201.0"]
-//   const Apache = ["Apache%202.0"]
-//   const Mozilla =["MPL%202.0"]
-//   const None = []
-//   return license
-//}
+ function renderLicenseBadge(licenseName) {
+  if (licenseName === "None"){
+    return "";
+  }
+  else{
+   const licenseData = licenseName === "IBM" ? "IPL%201.0": licenseName === "Apache"? "Apache%202.0": licenseName ==="Mozilla"? "MPL%202.0": "MIT";  
+ 
+    return `![License](https://img.shields.io/badge/License-${licenseData}-green.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+function renderLicenseLink(licenseName) {
+  if (licenseName === "None"){
+    return "";
+  }
+  else{
+   const licenseText = licenseName === "IBM" ? "https://opensource.org/licenses/IPL-1.0": licenseName === "Apache"? "https://opensource.org/licenses/Apache-2.0": licenseName ==="Mozilla"? "https://opensource.org/licenses/MPL-2.0": "https://opensource.org/licenses/MITT";  
+ 
+    return `${licenseText}`
+  }
+}
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
 
 
 // TODO: Create a function to generate markdown for README
@@ -45,8 +58,10 @@ ${data.contributors}
 ${data.tests}
 
 # License
-![License](https://img.shields.io/badge/License-${data.licenses}-green.svg)
-This is application is covered under the ${data.licenses} license.
+${renderLicenseBadge(data.licenses)}
+
+*This is application is covered under the ${data.licenses} license. Follow the link for more information about the license.
+*${renderLicenseLink(data.licenses)}
 
 # Questions?
 * https://github.com/${data.gitHubUserName}
